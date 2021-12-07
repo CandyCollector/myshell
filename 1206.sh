0e10c58452578822cacd85fa2ac7d5c76bcad537
tuanzhang <tuanzhang87@gmail.com>
2021-12-07 19:38:39 +0800

    1206

--- 1206.sh
+++ 1206.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+set -e
+
 # 批量创建用户
-cat user.txt
-aaaa,111
-vbbb,22222
-vvvvv,1111
-111,2222
-111,2222
+# [root@i-ivipbag1 shell] cat user.txt 
+# aaaa,111
+# vbbb,22222
+# vvvvv,1111
+# 111,2222
+# 111,2222
+
+while read line ;
+do
+    if [ `echo $line |awk -F ',' {'print $1'} |xargs useradd &>/dev/null`]; then 
+        `echo $line |awk -F ',' {'print $2'} |passwd --stdin $1 >dev/null 2&>1`
+    else
+        echo "used already exsis"
+    fi
+done < user.txt
